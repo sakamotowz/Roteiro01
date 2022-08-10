090b309094d6b21cbc37175c3d1b5c406a69f1f0
Sr-Cardoso <nathancar.linhares77@gmail.com>
2022-08-10 20:25:38 -0300

    Código criado na sala

---
+++ questao01.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include<time.h>
+
+int algoritmoDemorado(int n);
+
+int main(){
+    clock_t t1, soma = 0;
+    int x = 10;
+    t1 = clock();
+    for(int i = 0; i < x; i++){
+        t1 = clock();
+        algoritmoDemorado(200);
+        t1 = clock() - t1;
+        soma += t1;
+    }
+    printf("O tempo foi %f\n",(((float)soma)/x)/CLOCKS_PER_SEC);
+    
+return 0;
+}
+int algoritmoDemorado (int n){
+    int soma = 0;
+    for (int i = 0; i < n; i++){
+        /*if( i % 100 == 0){
+            printf("1 = %i\n\n",i);
+        }*/
+        for(int j = 0; j < n; j++){
+            for(int k = 0; k < n; k++){
+                for(int l = 0; l < n; l++){
+                    soma = i + j + k + l;
+                }
+            }
+        }
+    }
+return soma;
+}
